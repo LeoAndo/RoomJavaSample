369fa2e051f9610e0d64437fcfb64e5241c604ce
ando <leo81@outlook.jp>
2023-07-15 00:14:44 +0900

    update

--- app/src/main/java/com/leoleo/roomjavasample/roomjavasample/data/DataModule.java
+++ app/src/main/java/com/leoleo/roomjavasample/roomjavasample/data/DataModule.java
@@ -6,6 +6,10 @@ import androidx.room.Room;
 
 import com.leoleo.roomjavasample.roomjavasample.Logger;
 
+/**
+ * @see <a href="https://en.wikipedia.org/wiki/Double-checked_locking#Usage_in_Java">Double-checked_locking</a>
+ * @see <a href="https://en.wikipedia.org/wiki/Singleton_pattern">Singleton_pattern</a>
+ */
 public class DataModule {
     private static volatile DataModule dataModule;
     private volatile AppDatabase appDatabase;
